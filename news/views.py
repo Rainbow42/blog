1a97d149e99f2bfb1152fe27eab6352e70192575
antonida <anotnida619@gmail.com>
2020-07-23 14:48:18 +0400

    Update setting admin for News

--- news/views.py
+++ news/views.py
@@ -6,5 +6,5 @@ from .models import News
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список нововстей'})
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
