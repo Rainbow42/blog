83d70bbb10e218e1ec14371bd9cc6f7936099fcf
antonida <anotnida619@gmail.com>
2020-09-16 10:49:24 +0400

    Chenges requirements

--- news/models.py
+++ news/models.py
@@ -22,6 +22,9 @@ class News(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
